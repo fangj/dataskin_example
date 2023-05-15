11a47c0d5626db53aab70f2773497cda43c19771
fangjian <fangjian@126.com>
2023-05-15 09:22:12 +0800

    antd@5 ok

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button, ConfigProvider } from 'antd';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,9 +19,18 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <ConfigProvider
+          theme={{
+            token: {
+              colorPrimary: '#00b96b',
+            },
+          }}
+        >
+          <Button type='primary' >定制主题</Button>
+        </ConfigProvider>
+        <Button type='primary' onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
--- steps.md
+++ steps.md
@@ -13,7 +13,12 @@ npx express-generator server --view=ejs
 ## 运行
 
 * 客户端
-`pnpm run dev`
+`npm run dev`
 
 * 服务端
 `SET DEBUG=server:* & npm start`
+
+## 引入antd 5
+
+`yarn add antd@^5`
+
