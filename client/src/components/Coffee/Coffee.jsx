e1d9944836411250061426f239156f9da84c98b5
fangjian <fangjian@126.com>
2023-05-17 16:17:47 +0800

    add animation ok

--- client/src/components/Coffee/Coffee.jsx
+++ client/src/components/Coffee/Coffee.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { Card, Statistic,Button } from 'antd';
+import { makeAutoObservable, autorun } from "mobx"
 import { observer } from "mobx-react-lite"
 import CoffeeSvg  from "./coffee.svg";
 import UnitStore from "../../stores/UnitStore";
 import "./coffee.css";
-
+import love from "./love.webm";
 const store=new UnitStore("coffee");
+
+autorun(() => {
+    console.log(store.value.total)
+    document.getElementById("v1").play()
+})
 
 function CoffeePage() {
     return(
     <div className="coffee_bg ">
-        <div className="page_container">
-        <Card bordered={false} style={{width:250, margin:"0 auto"}}>
+        <div className="page_container" style={{position:"relative"}}>
+       
+        <Card bordered={false} style={{width:250,height:400, margin:"0 auto",position:"relative"}}>
         <img src={CoffeeSvg} />
             <Statistic
                 title="销量"
@@ -21,6 +28,9 @@ function CoffeePage() {
                 }}
                 suffix="杯"
             />
+              <video  width="200" height="400" muted   preload="auto" id="v1" 
+            src={love} type="video/webm" style={{position:"absolute",left:0,top:0}}>
+        </video>
         </Card>
         </div>
     </div>)
